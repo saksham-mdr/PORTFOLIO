a140895270f6cca12dff78554200ceb3c6ed827f
saksham <79905837+saksham-mdr@users.noreply.github.com>
2022-06-19 14:10:03 +0545

    Add files via upload

---
+++ portfolio/index.js
@@ -0,0 +1,19 @@
+const cks = document.getElementById('ck');
+
+cks.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+    document.body.classList.toggle('dark1');
+    document.body.classList.toggle('dark2');
+    document.body.classList.toggle('dark3');
+    document.body.classList.toggle('dark4');
+    document.body.classList.toggle('dark5');
+});
+
+const abc =document.getElementById('devs');
+
+abc.addEventListener('change', () => {
+        document.body.classList.toggle('hov');
+
+    });
+   
+    
